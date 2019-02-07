18a1c79c41ade644b03b7080b4e7ebfff61c4bc6
André Sousa <andreosousa95@gmail.com>
2019-02-07 19:12:28 -0100

    Fixed path to get today's matches

--- handler.go
+++ handler.go
@@ -7,6 +7,7 @@ import (
 	"lol-esports-calendar/models"
 	"lol-esports-calendar/utils"
 	"net/http"
+	"time"
 )
 
 const (
@@ -25,12 +26,13 @@ func initConfig() {
 // Gets today's professional LoL matches through the Pandascore API
 func getTodayMatches() {
 	var (
-		queryURL string
-		matches  models.Matches
+		queryURL    string
+		matches     models.Matches
+		currentDate string
 	)
 
-	// currentDate = time.Now().Local().Format("2006-01-02")
-	queryURL = basePandascoreURL + "matches/past?token=" + config.APIKey
+	currentDate = time.Now().Local().Format("2006-01-02")
+	queryURL = basePandascoreURL + "matches/upcoming?token=" + config.APIKey + "&filter[begin_at]=" + currentDate
 
 	res, err := http.Get(queryURL)
 	if err != nil {
